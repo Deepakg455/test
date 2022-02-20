bb36cd1acd1f8ddd9bbee8993c2d9ff7424ab941
Deepakg455 <deepak.g455@gmail.com>
2022-02-21 01:04:33 +0530

    added  myFirstGitFile.java second time

--- myFirstGitFile.java
+++ myFirstGitFile.java
@@ -3,4 +3,8 @@ public myFirstGitFile{
 	public static void main(String args[]){
 		Sysout.out.println("Hello GitHub");
 	}
+	
+	public static void printHello(){
+		Sysout.out.println("First Program to commit on github");
+	}
 }
